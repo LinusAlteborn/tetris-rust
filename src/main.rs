20df353230e30d456035e2fdcbfeb5870631ad56
Carl <meatballsofmeatballs@gmail.com>
2023-04-26 22:17:54 +0200

    la till json support

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use tetris::{*, io::input::InputEvent};
 use std::time::{Duration};
 use crossterm::event::{read, Event, KeyCode};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{cursor, terminal, ExecutableCommand, Result};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std::io::{stdout, Write};
+use std::fs;
 
 
 
@@ -10,87 +14,103 @@ struct Settings {
     color: char,
 }
 
+struct User {
+    name: String,
+    score: u32
+}
+
+struct Highscores {
+    users: Vec<User>
+}
+
 impl Settings {
     fn start() -> crossterm::Result<Settings> {
-        println!(
-            "Welcome to Tetris\n\nPress 1. to play\nPress 2. for settings\nPress 3. for highscore"
-        );
-
-        enable_raw_mode().unwrap();
-        let result = loop {
-            if let Event::Key(key) = read().unwrap() {
-                match key.code {
-                    KeyCode::Char('1') => break 1,
-                    KeyCode::Char('2') => break 2,
-                    KeyCode::Char('3') => break 3,
-                    _ => (),
-                }
-            }
-        };
-        disable_raw_mode().unwrap();
+        let content = fs::read_to_string("./src/settings.json").expect("error");
+        let json = json::parse(&content).unwrap();
+        let mut result = -1;
         let mut settings = Settings {
-            difficulty: 1,
-            color: 'b',
-        };
-        if result == 1 {
-            return Ok(settings);
-        } else if result == 2 {
-            let mut result = -1;
-            loop {
-                if result == -1 {
-                    println!("Settings\n\nPress 1. for difficulty\nPress 2. for color\nPress 3. for exit");
-                    result = 0;
-                } else if result == 0 {
-                    enable_raw_mode().unwrap();
-                    if let Event::Key(key) = read().unwrap() {
-                        match key.code {
-                            KeyCode::Char('1') => result = 1,
-                            KeyCode::Char('2') => result = 2,
-                            KeyCode::Char('3') => break,
-                            _ => (),
-                        }
+                difficulty: json["Difficulty"].as_u32().unwrap(),
+                color: json["color"].as_str().unwrap().chars().nth(0).unwrap(),
+            };
+            println!("{}", settings.difficulty);
+        loop {
+            if result == -1{
+                disable_raw_mode().unwrap();
+                println!(
+                    "Welcome to Tetris\n\nPress 1. to play\nPress 2. for settings\nPress 3. for highscore"
+                );
+                enable_raw_mode().unwrap();
+                if let Event::Key(key) = read().unwrap() {
+                    match key.code {
+                        KeyCode::Char('1') => result = 1,
+                        KeyCode::Char('2') => result = 2,
+                        KeyCode::Char('3') => result = 3,
+                        _ => (),
                     }
-                    disable_raw_mode().unwrap();
-                }
-                if result == 1 {
-                    println!("Set difficulty in the range 1-9");
-                    enable_raw_mode().unwrap();
-                    let difficulty: Option<char> = loop {
-                        if let Event::Key(key) = read().unwrap() {
-                            match key.code {
-                                KeyCode::Char(event) => {
-                                    if event.is_digit(10) {
-                                        break Some(event);
-                                    }
-                                }
-                                _ => (),
-                            }
-                        }
-                    };
-                    result = -1;
-                    disable_raw_mode().unwrap();
-                    settings.difficulty = difficulty.unwrap().to_digit(10).unwrap();
-                }
-                if result == 2 {
-                    println!("Set color");
-                    enable_raw_mode().unwrap();
-                    let color: Option<char> = loop {
-                        if let Event::Key(key) = read().unwrap() {
-                            match key.code {
-                                KeyCode::Char('r') => break Some('r'),
-                                KeyCode::Char('b') => break Some('b'),
-                                _ => (),
-                            }
-                        }
-                    };
-                    result = -1;
-                    disable_raw_mode().unwrap();
-                    settings.color = color.unwrap()
                 }
             }
             disable_raw_mode().unwrap();
+            if result == 1 {
+                return Ok(settings);
+            } else if result == 2 {
+                let mut settings_result = -1;
+                loop {
+                    if settings_result == -1 {
+                        println!("Settings\n\nPress 1. for difficulty\nPress 2. for color\nPress 3. for exit");
+                        settings_result = 0;
+                    } else if settings_result == 0 {
+                        enable_raw_mode().unwrap();
+                        if let Event::Key(key) = read().unwrap() {
+                            match key.code {
+                                KeyCode::Char('1') => settings_result = 1,
+                                KeyCode::Char('2') => settings_result = 2,
+                                KeyCode::Char('3') => break,
+                                _ => (),
+                            }
+                        }
+                        disable_raw_mode().unwrap();
+                    }
+                    if settings_result == 1 {
+                        println!("Set difficulty in the range 1-9");
+                        enable_raw_mode().unwrap();
+                        let difficulty: Option<char> = loop {
+                            if let Event::Key(key) = read().unwrap() {
+                                match key.code {
+                                    KeyCode::Char(event) => {
+                                        if event.is_digit(10) {
+                                            break Some(event);
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                        };
+                        settings_result = -1;
+                        disable_raw_mode().unwrap();
+                        settings.difficulty = difficulty.unwrap().to_digit(10).unwrap();
+                    }
+                    if settings_result == 2 {
+                        println!("Set color");
+                        enable_raw_mode().unwrap();
+                        let color: Option<char> = loop {
+                            if let Event::Key(key) = read().unwrap() {
+                                match key.code {
+                                    KeyCode::Char('r') => break Some('r'),
+                                    KeyCode::Char('b') => break Some('b'),
+                                    _ => (),
+                                }
+                            }
+                        };
+                        settings_result = -1;
+                        disable_raw_mode().unwrap();
+                        settings.color = color.unwrap()
+                    }
+                }
+                result = -1;
+                disable_raw_mode().unwrap();
+            }
+        
         }
-        Ok(settings)
     }
 }
 
